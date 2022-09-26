974d4d78485f13537e5cb7d7021e8405e181cf59
thiyagu10 <thiyaguitx@gmail.com>
2022-09-26 09:24:46 +0530

    Update installOnUbuntu.sh

--- installOnUbuntu.sh
+++ installOnUbuntu.sh
@@ -8,14 +8,18 @@ sudo apt update -y
 sudo apt install inetutils-ping inetutils-traceroute vim wget tcpdump net-tools iperf3 ifupdown lshw -y
 
 cat << EOF | tee /etc/hostname
-PP-VPP01
+PP-VPP02
 EOF
+cat << EOF | tee  /etc/network/interfaces
+# interfaces(5) file used by ifup(8) and ifdown(8)
+# Include files from /etc/network/interfaces.d:
+source-directory /etc/network/interfaces.d
 
-sed -i 's/127.0.1.1/192.168.56.151\tPP-VPP01/g' /etc/hosts
-
-mkdir -p /var/log/vpp/
-cat << EOF | tee /var/log/vpp/vpp.log
-### VPP Logs
+allow-hotplug enp0s3
+iface enp0s3 inet static
+             address 192.168.56.152/24
+allow-hotplug enp0s8
+iface enp0s8 inet dhcp
 EOF
 
 cat << EOF | tee /etc/network/interfaces.d/dpdk-interfaces.conf
@@ -23,7 +27,14 @@ cat << EOF | tee /etc/network/interfaces.d/dpdk-interfaces.conf
 allow-hotplug enp0s9
 allow-hotplug enp0s10
 EOF
+rm -rf /etc/netplan/*.yaml
 systemctl restart networking
+sed -i 's/127.0.1.1/192.168.56.152\tPP-VPP02/g' /etc/hosts
+
+mkdir -p /var/log/vpp/
+cat << EOF | tee /var/log/vpp/vpp.log
+### VPP Logs
+EOF
 
 cat <<EOF | sudo tee /etc/apt/sources.list.d/99fd.io.list
 deb [trusted=yes] https://packagecloud.io/fdio/release/ubuntu focal main
@@ -32,5 +43,4 @@ curl -L https://packagecloud.io/fdio/release/gpgkey | sudo apt-key add -
 
 sudo apt-get update -y
 sudo apt-get install vpp vpp-plugin-core vpp-plugin-dpdk dpdk -y
-
 systemctl start dpdk
